595723412fcac881c24c499bb252f198976ca7ad
Sam <dsaeresearch@gmail.com>
2020-10-28 13:02:29 -0400

    Add project files.

---
+++ PlayingAndTestingC/Main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+void SayHello() {
+
+	printf("hello\n");
+	/*
+	%c character
+	%d interger number (base10)
+	%e expontential floatin-point number
+	%f floating-point number
+	%i integer (base 10)
+	%o octal number (base 8)
+	%s string of characters
+	%u unsigned decimal (integer) number
+	%x number in hexadecimal (base 16)
+	%% print a percent sign
+	\% print a percent sign
+	*/
+}
+
+void main(int argc, char** argv) {
+	SayHello();
+	//changes more
+}
